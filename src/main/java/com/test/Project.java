ae79c1c5e211bfe8eaf835added7e246244100c5
kamyshev <kamyshev@localhost.localdomain>
2017-07-26 16:11:19 +0300

    change src

--- src/main/java/com/test/Project.java
+++ src/main/java/com/test/Project.java
@@ -8,7 +8,7 @@ class Project {
   public static void test() {
     System.out.println("test");
   }
-  
+  public static void test (1)  
   public static void main(String[] args) {
     System.out.println("Hellow MTN");
   }
