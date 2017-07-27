fad73dbac9b0b7f4ba1c5599f74dcfe85c944604
kamyshev <kamyshev@localhost.localdomain>
2017-07-27 14:32:47 +0300

    java correct

--- src/main/java/com/test/Project.java
+++ src/main/java/com/test/Project.java
@@ -8,8 +8,7 @@ class Project {
   public static void test() {
     System.out.println("test");
   }
-  public static void test1(){
-}  
+
   public static void main(String[] args) {
     System.out.println("Hellow MTN");
   }
